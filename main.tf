6eb6aff85e4e5bfc5aeceb99ae42dfad33beef57
nithinsettibathula <nithinsece58@gmail.com>
2026-02-20 15:19:44 +0530

    Deploying Strapi with CloudWatch Monitoring

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 1. VPC and Subnets (Default resources fetch cheyadam)
+# 1. VPC and Subnets
 data "aws_vpc" "default" {
   default = true
 }
@@ -14,13 +14,13 @@ data "aws_subnets" "default" {
   }
 }
 
-# 2. CloudWatch Log Group (Instruction 2: Logging setup)
+# 2. CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "strapi" {
   name              = "/ecs/strapi-nithin"
   retention_in_days = 7
 }
 
-# 3. ECS Cluster with Container Insights (Instruction 3: Monitoring/Metrics)
+# 3. ECS Cluster with Container Insights
 resource "aws_ecs_cluster" "strapi_cluster" {
   name = "strapi-cluster-nithin"
 
@@ -30,7 +30,7 @@ resource "aws_ecs_cluster" "strapi_cluster" {
   }
 }
 
-# 4. Security Group (Port 1337 open cheyadam)
+# 4. Security Group
 resource "aws_security_group" "strapi_sg" {
   name        = "strapi-sg-nithin"
   description = "Allow Strapi traffic"
@@ -55,7 +55,7 @@ resource "aws_security_group" "strapi_sg" {
 resource "aws_ecs_service" "strapi_service" {
   name            = "strapi-service-nithin"
   cluster         = aws_ecs_cluster.strapi_cluster.id
-  task_definition = "strapi-task" # Matches family name in task-definition.json
+  task_definition = "strapi-task" 
   launch_type     = "FARGATE"
   desired_count   = 1
 
@@ -64,4 +64,44 @@ resource "aws_ecs_service" "strapi_service" {
     assign_public_ip = true
     security_groups  = [aws_security_group.strapi_sg.id]
   }
+}
+
+# 6. CloudWatch Dashboard
+resource "aws_cloudwatch_dashboard" "strapi_dashboard" {
+  dashboard_name = "Strapi-Monitoring-Nithin"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/ECS", "CPUUtilization", "ServiceName", "strapi-service-nithin", "ClusterName", "strapi-cluster-nithin" ],
+            [ "AWS/ECS", "MemoryUtilization", "ServiceName", "strapi-service-nithin", "ClusterName", "strapi-cluster-nithin" ]
+          ]
+          period = 60
+          stat   = "Average"
+          region = "us-east-1"
+          title  = "CPU and Memory Utilization"
+        }
+      },
+      {
+        type   = "metric"
+        width  = 12
+        height = 6
+        properties = {
+          metrics = [
+            [ "AWS/ECS", "NetworkRxBytes", "ServiceName", "strapi-service-nithin", "ClusterName", "strapi-cluster-nithin" ],
+            [ "AWS/ECS", "NetworkTxBytes", "ServiceName", "strapi-service-nithin", "ClusterName", "strapi-cluster-nithin" ]
+          ]
+          period = 60
+          stat   = "Average"
+          region = "us-east-1"
+          title  = "Network In/Out"
+        }
+      }
+    ]
+  })
 }
